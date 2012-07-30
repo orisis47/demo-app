94b003859cf5ebfb99eae44a0c499d8ba25def2f
multunus <midhun.k@multunus.com>
2012-07-30 12:44:16 +0530

    demo app initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '3a5a7ebef7a628b8b29dc8947020a53eef25bbfe33df4c3cc7e450fca1a7ed19ce9eb91d4371e7f7422120ea8b80074f6ab71dbe967144059d2d8228e592a64e'
